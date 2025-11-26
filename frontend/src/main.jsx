cb5beed9bbf71b8e350493874d59f7b98d032de8
Bhagya Karunanayake <2003zerokali@gmail.com>
2025-11-26 12:45:36 +0530

    Fix import typos and format JSX structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-impport {BrowserRouter} from 'react-router-dom'
-import StroeContextProvider from './context/StoreContext'
+import {BrowserRouter} from 'react-router-dom'
+import StoreContextProvider from './context/StoreContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<StoreContextProvider>
-			<App/>
-		</StoreContextProvider>
-	</BrowserRouter>
+  <BrowserRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>
 )
-
