fe7a96fd17c06fe7ae4c571f8d71c65292c7894d
Bhagya <2003zerokali@gmail.com>
2025-12-06 20:34:57 +0530

    modified server.js until work with package.json

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+
+
+
+//app config
+const app = express()
+const port = 4000
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+	res.send("API Working")
+})
+
+app.listen(port,()=>{
+	console.log(`Server Strated on http://localhost:${port}`)
+})
+
+
+
