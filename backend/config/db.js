5f4a35bc0d0e5a7847bbf0044c44605ecf2cd83d
Bhagya <2003zerokali@gmail.com>
2025-12-09 23:01:59 +0530

    update db.js for password in cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=> {
-	await mongoose.connect('mongodb+srv://2003zerokali20_db_user:<db_password>@cluster0.z10r6lj.mongodb.net/Tomato-restaurant-').then(()=>console.log ("DB Connected"));
+	await mongoose.connect('mongodb+srv://2003zerokali20_db_user:20031220@cluster0.z10r6lj.mongodb.net/Tomato-restaurant-').then(()=>console.log ("DB Connected"));
 
 }
