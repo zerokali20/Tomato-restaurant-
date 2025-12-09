3e83582d2cdab0a0cafa58fcce82515ea3d06ccf
Bhagya <2003zerokali@gmail.com>
2025-12-09 22:57:14 +0530

    update db.js for password in database cluster and server.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 
-export const connectDB = async()=> {
-	await mongoose.connect('mongodb+srv://2003zerokali20_db_user:<db_password>@cluster0.z10r6lj.mongodb.net/Tomato-restaurant-').then(()=>console.log ("DB Connected"));
+export const connectDB = async () => {
+	try {
+		const envUri = process.env.MONGO_URI;
+		const user = process.env.DB_USER;
+		const pass = process.env.DB_PASS;
+		const dbName = process.env.DB_NAME || "Tomato-restaurant-";
 
-}
+		const uri = envUri
+			? envUri
+			: `mongodb+srv://${user}:${encodeURIComponent(pass)}@cluster0.z10r6lj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+		await mongoose.connect(uri);
+		console.log("DB Connected");
+	} catch (err) {
+		console.error("DB connection error:", err.message || err);
+		process.exit(1);
+	}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
 import {connectDB} from "./config/db.js"
+
+dotenv.config()
 
 
 //app config
